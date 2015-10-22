f48264e46192fb2b908da01d2e7935e2012864d5
yminowa <yminowa@terrasky.co.jp>
2015-10-22 10:39:25 +0900

    linkタグ修正

--- tasks/visualforce.js
+++ tasks/visualforce.js
@@ -1,6 +1,6 @@
 module.exports = function (name) {
   return `<apex:page showHeader="false" standardStylesheets="false">
-  <link href="/resource/SLDS/styles/salesforce-lightning-design-system-scoped.min.css" />
+  <link rel="stylesheet" type="text/css" href="/resource/SLDS/styles/salesforce-lightning-design-system-scoped.min.css" />
   <div id="content" class="slds"></div>
   <script src="/resource/${name}"></script>
 </apex:page>`;
